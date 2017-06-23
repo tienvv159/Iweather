f812a1a2a262a11d8dbcbb269d95c13bbe708fd8
Vu Van Tien <mrtienvu123@gmail.com>
2017-06-23 09:46:13 +0700

    no message

---
+++ iweather/iweather/MVC/CellDetail/MyCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCellTableViewCell.swift
+//  iweather
+//
+//  Created by Vu Van Tien on 6/22/2560 BE.
+//  Copyright © 2560 BE MyStudio. All rights reserved.
+//
+
+import UIKit
+
+class MyCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ iweather/iweather/MVC/CellDetail/MyHeaderInSection.swift
@@ -0,0 +1,35 @@
+//
+//  MyHeaderInSection.swift
+//  iweather
+//
+//  Created by Vu Van Tien on 6/22/2560 BE.
+//  Copyright © 2560 BE MyStudio. All rights reserved.
+//
+
+import UIKit
+
+class MyHeaderInSection: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
