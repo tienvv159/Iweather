0104993d5bdf8d5eb8ddc9e243f877305d91b309
Vu Van Tien <mrtienvu123@gmail.com>
2017-06-28 23:55:41 +0700

    no message

---
+++ iweather/iweather/MVC/CellDetail/myCellTempHour.swift
@@ -0,0 +1,17 @@
+//
+//  myCollectionTempHour.swift
+//  iweather
+//
+//  Created by Vu Van Tien on 6/28/2560 BE.
+//  Copyright © 2560 BE MyStudio. All rights reserved.
+//
+
+import UIKit
+
+class myCollectionTempHour: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
